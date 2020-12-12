c73b7a686cfe3910e58fb6b783fc89088d04f348
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-12-12 13:47:09 +0530

    Image conversion

--- lib/src/converter.dart
+++ lib/src/converter.dart
@@ -1,1 +1,23 @@
+import 'dart:io';
+import 'package:image/image.dart';
 
+String convertImage(FileSystemEntity selectedFile, String format) {
+  final List<int> rawImage = (selectedFile as File).readAsBytesSync();
+  final Image image = decodeImage(rawImage);
+
+  var newImage;
+  if (format == 'jpeg') {
+    newImage = encodeJpg(image);
+  } else if (format == 'png') {
+    newImage = encodePng(image);
+  } else {
+    stdout.writeln('Error: Unsupported file type.');
+  }
+  final String newPath = replaceExtension(selectedFile.path, format);
+  File(newPath).writeAsBytesSync(newImage);
+  return newPath;
+}
+
+String replaceExtension(String path, String newExtension) {
+  return path.replaceAll(RegExp(r'(png|jpg|jpeg)'), newExtension);
+}
